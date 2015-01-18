2ae8550b23494206a673f0587addc339212ba07f
Jochen Stals <stalski@322618.no-reply.drupal.org>
2015-01-18 12:39:39 +0100

    upgraded drupal to 7.34.

--- www/sites/all/themes/creating_bootstrap/template.php
+++ www/sites/all/themes/creating_bootstrap/template.php
@@ -12,11 +12,10 @@
 function creating_bootstrap_preprocess_page(&$variables) {
 
   // Primary nav.
-  $config = menu_block_get_config('1');
-  $data = menu_tree_build($config);
-  $variables['primary_nav'] = $data['content'];
-  $variables['primary_nav']['#theme_wrappers'] = array('menu_tree__primary');
-
+  $data = menu_tree_all_data('main-menu');
+  $tree = menu_tree_output($data);
+  $tree['#theme_wrappers'] = array('menu_tree__primary');
+  $variables['primary_nav'] = drupal_render($tree);
 
 }
 
